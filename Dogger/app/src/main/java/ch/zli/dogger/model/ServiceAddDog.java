b32d7a7b0cdb628f85bfeb345f024dd9af669d8b
oscar-github <oscar.martinez@jobcloud.ch>
2021-01-21 14:54:56 +0100

    renamed packages

--- Dogger/app/src/main/java/ch/zli/dogger/model/ServiceAddDog.java
+++ Dogger/app/src/main/java/ch/zli/dogger/model/ServiceAddDog.java
@@ -1,11 +1,11 @@
-package ch.zli.dogger.data;
+package ch.zli.dogger.model;
 
 import android.content.Intent;
 import android.os.IBinder;
 
 import androidx.annotation.Nullable;
 
-public class ServiceMap extends android.app.Service {
+public class ServiceAddDog extends android.app.Service {
 
     @Nullable
     @Override
