c9224c3d392c31acec334ad9265a534868384ba0
oscar-github <oscar.martinez@jobcloud.ch>
2021-01-21 09:23:27 +0100

    created new ServiceMap

---
+++ Dogger/app/src/main/java/ch/zli/dogger/data/ServiceMap.java
@@ -0,0 +1,17 @@
+package ch.zli.dogger.data;
+
+import android.content.Intent;
+import android.os.IBinder;
+
+import androidx.annotation.Nullable;
+
+public class ServiceMap extends android.app.Service {
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+
+}
